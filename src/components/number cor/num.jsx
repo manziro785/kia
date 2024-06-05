648eaacf5df019cdcb6f05c65736c670d60500a5
Adilet25 <baryktabasovadi@gmail.com>
2024-06-05 19:39:15 +0600

    added new section,tour in numbers

--- src/components/number cor/num.jsx
+++ src/components/number cor/num.jsx
@@ -1,204 +1,124 @@
-import React, { useState, useEffect } from 'react';
-import './num.css';
-import imm from '../../assets/calendar/Vector (2).svg'
+import React, { useState, useEffect } from "react";
+import "./num.css";
+import imm from "../../assets/calendar/Vector (2).svg";
 
-import imm2 from '../../assets/corusel/1.1.svg'
-import imm3 from '../../assets/corusel/1.2.svg'
+import imm2 from "../../assets/corusel/1.1.svg";
+import imm3 from "../../assets/corusel/1.2.svg";
+import { Autoplay, Navigation, Pagination, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+import "swiper/css";
+import "swiper/css/pagination";
 
 const events = [
   {
-    date: 'June 22th-30th',
-    title: 'Regional Qualifiers Asia',
-    type: 'ONLINE',
-    backgroundImage: 'path-to-image-1.png',
+    date: "June 22th-30th",
+    title: "Regional Qualifiers Asia",
+    type: "ONLINE",
+    backgroundImage: "path-to-image-1.png",
   },
   {
-    date: 'July 6th-31th',
-    title: 'Regional Qualifiers Europe',
-    type: 'ONLINE',
-    backgroundImage: 'path-to-image-2.png',
+    date: "July 6th-31th",
+    title: "Regional Qualifiers Europe",
+    type: "ONLINE",
+    backgroundImage: "path-to-image-2.png",
   },
   {
-    date: 'Jul 25th - Aug 7th',
-    title: 'Regional Qualifiers America',
-    type: 'ONLINE',
-    backgroundImage: 'path-to-image-3.png',
+    date: "Jul 25th - Aug 7th",
+    title: "Regional Qualifiers America",
+    type: "ONLINE",
+    backgroundImage: "path-to-image-3.png",
   },
   {
-    date: 'August 17th - 21st',
-    title: 'Regional Qualifiers Africa',
-    type: 'OFFLINE',
-    backgroundImage: 'path-to-image-4.png',
+    date: "August 17th - 21st",
+    title: "Regional Qualifiers Africa",
+    type: "OFFLINE",
+    backgroundImage: "path-to-image-4.png",
   },
   {
-    date: 'September 6th - 14th',
-    title: 'Regional Qualifiers Asia',
-    type: 'OFFLINE',
-    backgroundImage: 'path-to-image-5.png',
+    date: "September 6th - 14th",
+    title: "Regional Qualifiers Asia",
+    type: "OFFLINE",
+    backgroundImage: "path-to-image-5.png",
   },
   {
-    date: 'November 11th - 19th',
-    title: 'World Esports Championship',
-    type: 'OFFLINE',
-    backgroundImage: 'path-to-image-6.png',
+    date: "November 11th - 19th",
+    title: "World Esports Championship",
+    type: "OFFLINE",
+    backgroundImage: "path-to-image-6.png",
   },
   {
-    date: 'December 1st - 10th',
-    title: 'Regional Qualifiers Australia',
-    type: 'OFFLINE',
-    backgroundImage: 'path-to-image-7.png',
+    date: "December 1st - 10th",
+    title: "Regional Qualifiers Australia",
+    type: "OFFLINE",
+    backgroundImage: "path-to-image-7.png",
   },
   {
-    date: 'December 15th - 20th',
-    title: 'Regional Qualifiers Antarctica',
-    type: 'ONLINE',
-    backgroundImage: 'path-to-image-8.png',
+    date: "December 15th - 20th",
+    title: "Regional Qualifiers Antarctica",
+    type: "ONLINE",
+    backgroundImage: "path-to-image-8.png",
   },
   // Add more events as needed
 ];
 
 export default function Num() {
-  const itemsPerPage = 10; // Number of items to display per page
-  const totalPages = Math.ceil(events.length / itemsPerPage);
-  const [currentPage, setCurrentPage] = useState(0);
-
-  const handlePrevClick = () => {
-    if (currentPage > 0) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
-
-  const handleNextClick = () => {
-    if (currentPage < totalPages - 1) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-
-
-
-  const currentEvents = events.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage);
-
-
-
-  useEffect(() => {
-    const photoContainers1 = document.querySelectorAll('.photo-container1');
-    const photoContainers2 = document.querySelectorAll('.photo-container2');
-    // const photoContainers3 = document.querySelectorAll('.photo-container3');
-    
-    const checkBox = (elements) => {
-      const trigger = window.innerHeight / 5 * 4;
-
-      elements.forEach((element) => {
-        const topOfBox = element.getBoundingClientRect().top;
-
-        if (topOfBox < trigger) {
-          element.classList.add('show');
-        } else {
-          element.classList.remove('show');
-        }
-      });
-    };
-
-    const handleScroll = () => {
-      checkBox(photoContainers1);
-      checkBox(photoContainers2);
-    //   checkBox(photoContainers3);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    // Вызываем handleScroll сразу после загрузки страницы, чтобы определить положение
-    handleScroll();
-    
-    return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-
-
   return (
     <>
-    <div className="wrapp-main">
-    <div className="wrapper-calen">
-    <div className="events-calendar1">
-      <div className="calendar-header" style={{marginBottom: '-50px',  marginTop: '300px'}}>
-        <h1 style={{fontSize: '50px'}}>ТУРНИР В ЦИФРАХ</h1>
-        <div className="navigation">
-          
-
-        </div>
-      </div>
-<div className="wrap-call">
-<button onClick={handlePrevClick} disabled={currentPage === 0}>
-            <img src={imm} alt="" />
-          </button>
-      <div className="events-slider-wrapper">
-      
-        <div className="events-slider" style={{ transform: `translateX(${-currentPage * 100}%)` }}>
-           
-            <div className="event" >
-                <div className="box1">
-                  <div className="left">
-                    <div className="ups">
-                      <span>1 000 000</span> сом
-                    </div>
-                    <div className="dd">
-                    Призовой фонд турнира
-                    </div>
-                  </div>
-                  <div className="right">
-                    <img src={imm2} alt="" />
-                  </div>
-                </div>
-            </div>
-            <div className="event" >
+      <div>
+        <h1 style={{ fontSize: "2rem", marginLeft: "5rem" }}>
+          ТУРНИР В ЦИФРАХ
+        </h1>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={0}
+          autoplay={{
+            delay: 4000,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay, Navigation, Pagination, A11y]}
+        >
+          <SwiperSlide>
             <div className="box1">
-                  <div className="left">
-                    <div className="ups">
-                      <span>50</span> команд
-                    </div>
-                    <div className="dd">
-                    500+ участников                    </div>
-                  </div>
-                  <div className="right">
-                    <img src={imm3} alt="" />
-                  </div>
+              <div className="left">
+                <div className="ups">
+                  <span>1 000 000</span> сом
                 </div>
+                <div className="dd">Призовой фонд турнира</div>
+              </div>
+              <div className="right">
+                <img src={imm2} alt="" />
+              </div>
             </div>
-            <div className="event" >
+          </SwiperSlide>
+          <SwiperSlide>
             <div className="box1">
-                  <div className="left">
-                    <div className="ups">
-                      <span>50</span> команд
-                    </div>
-                    <div className="dd">
-                    500+ участников                    </div>
-                  </div>
-                  <div className="right">
-                    <img src="./src/assets/corusel/1.2.svg" alt="" />
-                  </div>
+              <div className="left">
+                <div className="ups">
+                  <span>50</span> команд
                 </div>
+                <div className="dd">500+ участников </div>
+              </div>
+              <div className="right">
+                <img src={imm3} alt="" />
+              </div>
             </div>
-          
-          
-
-        
-        </div>
-        
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="box1">
+              <div className="left">
+                <div className="ups">
+                  <span>25</span> +
+                </div>
+                <div className="dd">Спонсоров</div>
+              </div>
+              <div className="right">
+                <img src="./src/assets/corusel/1.2.svg" alt="" />
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
-      <button onClick={handleNextClick} disabled={currentPage === totalPages - 1}>
-      <img src="./src/assets/calendar/btn-r.svg" alt="" />
-          </button>
-    </div></div>
-    <div className= 'photo-container1' >
-      {/* <img src="./src/assets/calendar/scorpion 1.svg" alt="Your Photo" className="scroll-photo" /> */}
-    </div>
-    </div>
-    </div>
-
     </>
-    
   );
 }
--- src/pages/mainPage/mainPage.jsx
+++ src/pages/mainPage/mainPage.jsx
@@ -1,48 +1,42 @@
-import React from 'react'
-import Calendar from '../../components/calendar/calendar'
-import Banner from '../../components/banner/banner'
-import Car from '../../components/car/car'
-import Num from '../../components/number cor/num'
-import Timer from '../../components/timer/timer'
-import GameSection from '../../components/gamesection/game'
-import Watch from '../../components/watch/watch'
-import Partners from '../../components/partners/partner'
-import cs22 from '../../assets/header/Frame 5 (2).svg'
-import FAQ from '../../components/questions/question'
-import Header from '../../components/header/header'
-import GameSection2 from '../../components/game2/game2'
-import Calendar2 from '../../components/calendar2/calendar2'
-
-
-
+import React from "react";
+import Calendar from "../../components/calendar/calendar";
+import Banner from "../../components/banner/banner";
+import Car from "../../components/car/car";
+import Num from "../../components/number cor/num";
+import Timer from "../../components/timer/timer";
+import GameSection from "../../components/gamesection/game";
+import Watch from "../../components/watch/watch";
+import Partners from "../../components/partners/partner";
+import cs22 from "../../assets/header/Frame 5 (2).svg";
+import FAQ from "../../components/questions/question";
+import Header from "../../components/header/header";
+import GameSection2 from "../../components/game2/game2";
+import Calendar2 from "../../components/calendar2/calendar2";
 
 export default function MainPage() {
   return (
     <>
-    <Header/>
+      <Header />
       <Banner />
 
-      <div className="wrap-calendar-sec" >
-    <Calendar/>
-    </div>
-    <div className="wrap-calendar2-sec" style={{display: 'none'}} >
-    <Calendar2/>
-    </div>
+      <div className="wrap-calendar-sec">
+        <Calendar />
+      </div>
+      <div className="wrap-calendar2-sec" style={{ display: "none" }}>
+        <Calendar2 />
+      </div>
 
-    <Car/>
-    {/* <Num/> */}
-    <div className="wrap-game-sec" style={{display: 'none'}}>
-    <GameSection/>
-
-    </div>
-    <div className="wrap-game-sec2">
-    <GameSection2/>
-
-    </div>
-    <Watch/>
-    <Partners/>
-    <FAQ/>
-
+      <Car />
+      <Num />
+      <div className="wrap-game-sec" style={{ display: "none" }}>
+        <GameSection />
+      </div>
+      <div className="wrap-game-sec2">
+        <GameSection2 />
+      </div>
+      <Watch />
+      <Partners />
+      <FAQ />
     </>
   );
 }
